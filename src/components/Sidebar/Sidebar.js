3118a2ea5afe845cb6f6b8d8f967937fc4243b8f
Amar <mamar8181@gmail.com>
2023-11-06 11:50:00 +0530

    sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,102 +1,224 @@
-import React, { useState } from "react";
-import "./Sidebar.css";
+import { NavLink } from "react-router-dom";
+import { FaBars, FaHome, FaLock, FaMoneyBill, FaUser } from "react-icons/fa";
+import { MdMessage } from "react-icons/md";
+import { BiAnalyse, BiSearch } from "react-icons/bi";
+import { BiCog } from "react-icons/bi";
+import { AiFillHeart, AiTwotoneFileExclamation } from "react-icons/ai";
+import { BsCartCheck } from "react-icons/bs";
+import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { FaBars, FaHome } from "react-icons/fa";
-import { Link, NavLink } from "react-router-dom";
+import SidebarMenu from "./SidebarMenu";
+import './Sidebar.css'
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "Dashboard",
     icon: <FaHome />,
   },
   {
-    path: "/services",
-    name: "Servicehome",
-    icon: <FaHome />,
+    path: "/users",
+    name: "Users",
+    icon: <FaUser />,
   },
   {
-    path: "/services",
-    name: "servicehome",
-    icon: <FaHome />,
+    path: "/messages",
+    name: "Messages",
+    icon: <MdMessage />,
   },
   {
-    path: "/ser",
-    name: "h",
-    icon: <FaHome />,
+    path: "/analytics",
+    name: "Analytics",
+    icon: <BiAnalyse />,
   },
   {
-    path: "/",
-    name: "Home",
-    icon: <FaHome />,
+    path: "/file-manager",
+    name: "File Manager",
+    icon: <AiTwotoneFileExclamation />,
+    subRoutes: [
+      {
+        path: "/settings/profile",
+        name: "Profile ",
+        icon: <FaUser />,
+      },
+      {
+        path: "/settings/2fa",
+        name: "2FA",
+        icon: <FaLock />,
+      },
+      {
+        path: "/settings/billing",
+        name: "Billing",
+        icon: <FaMoneyBill />,
+      },
+    ],
   },
   {
-    path: "/",
-    name: "Home",
-    icon: <FaHome />,
+    path: "/order",
+    name: "Order",
+    icon: <BsCartCheck />,
   },
   {
-    path: "/",
-    name: "Home",
-    icon: <FaHome />,
+    path: "/settings",
+    name: "Settings",
+    icon: <BiCog />,
+    exact: true,
+    subRoutes: [
+      {
+        path: "/settings/profile",
+        name: "Profile ",
+        icon: <FaUser />,
+      },
+      {
+        path: "/settings/2fa",
+        name: "2FA",
+        icon: <FaLock />,
+      },
+      {
+        path: "/settings/billing",
+        name: "Billing",
+        icon: <FaMoneyBill />,
+      },
+    ],
   },
   {
-    path: "/",
-    name: "Home",
-    icon: <FaHome />,
+    path: "/saved",
+    name: "Saved",
+    icon: <AiFillHeart />,
   },
 ];
-const Sidebar = ({ children }) => {
-  console.log(children);
-  const [isopen, setisOpen] = useState(false);
-  const toggle = () => {
-    setisOpen(!isopen);
+
+const SideBar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const inputAnimation = {
+    hidden: {
+      width: 0,
+      padding: 0,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    show: {
+      width: "140px",
+      padding: "5px 15px",
+      transition: {
+        duration: 0.2,
+      },
+    },
   };
+
+  const showAnimation = {
+    hidden: {
+      width: 0,
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    show: {
+      opacity: 1,
+      width: "auto",
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
-    <div className="main-container">
-      <motion.div
-        animate={{
-          width: isopen ? "250px" : "37px",
-          transition: {
-            duration: 0.5,
-            type: "spring",
-            damping: 10,
-          },
-        }}
-        className="sidebar"
-      >
-        <div className="top-section">
-          <div className="bars">
-            <FaBars onClick={toggle} />
+    <>
+      <div className="main-container">
+        <motion.div
+          animate={{
+            width: isOpen ? "250px" : "45px",
+
+            transition: {
+              duration: 0.5,
+              type: "spring",
+              damping: 10,
+            },
+          }}
+          className={`sidebar `}
+        >
+          <div className="top_section">
+            <AnimatePresence>
+              {isOpen && (
+                <motion.h1
+                  variants={showAnimation}
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  className="logo"
+                >
+                  DoSomeCoding
+                </motion.h1>
+              )}
+            </AnimatePresence>
+
+            <div className="bars">
+              <FaBars onClick={toggle} />
+              </div>
           </div>
-        </div>
+          {/*<div className="search">
+            <div className="search_icon">
+              <BiSearch />
+            </div>
+            <AnimatePresence>
+              {isOpen && (
+                <motion.input
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  variants={inputAnimation}
+                  type="text"
+                  placeholder="Search"
+                />
+              )}
+            </AnimatePresence>
+              </div>*/}
+          <section className="routes">
+            {routes.map((route, index) => {
+              if (route.subRoutes) {
+                return (
+                  <SidebarMenu
+                    setIsOpen={setIsOpen}
+                    route={route}
+                    showAnimation={showAnimation}
+                    isOpen={isOpen}
+                  />
+                );
+              }
 
-        <div>
-          {isopen && (
-            <h3 className="salon-name">
-              Monsoon salon Monsoon salon Monsoon salon Monsoon salon Monsoon
-              salon Monsoon salon Monsoon salon Monsoon salon Monsoon salon
-              Monsoon salon Monsoon salon Monsoon salon
-            </h3>
-          )}
-        </div>
+              return (
+                <NavLink
+                  to={route.path}
+                  key={index}
+                  className="link"
+                  activeClassName="active"
+                >
+                  <div className="icon">{route.icon}</div>
+                  <AnimatePresence>
+                    {isOpen && (
+                      <motion.div
+                        variants={showAnimation}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className="link_text"
+                      >
+                        {route.name}
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </NavLink>
+              );
+            })}
+          </section>
+        </motion.div>
 
-        <section className="routes">
-          {routes.map((route) => (
-            <NavLink
-              to={route.path}
-              key={route.name}
-              className="link"
-              activeClassName="active"
-            >
-              <div className="icon">{route.icon}</div>
-              <div className="link-text">{route.name}</div>
-            </NavLink>
-          ))}
-        </section>
-      </motion.div>
-      <main>{children}</main>
-    </div>
+        <main>{children}</main>
+      </div>
+    </>
   );
 };
 
-export default Sidebar;
+export default SideBar;
